17eaf1762de3170d862adecf438e9b4f32049827
Flier Lu <flier.lu@gmail.com>
2017-03-19 23:32:22 +0800

    install boost/hyperscan to system

--- .travis/build-hyperscan.sh
+++ .travis/build-hyperscan.sh
@@ -3,28 +3,26 @@ set -e
 
 if [ ! -f "$HYPERSCAN_ROOT/lib/libhs.a" ]; then
 	wget https://github.com/01org/hyperscan/archive/v$HYPERSCAN_VERSION.tar.gz -O /tmp/hyperscan.tar.gz
-	tar -xzf /tmp/hyperscan.tar.gz
-	cd hyperscan-$HYPERSCAN_VERSION
+	tar -xzf /tmp/hyperscan.tar.gz -C $HYPERSCAN_ROOT --strip-components 1
+	cd $HYPERSCAN_ROOT
 	rm -rf tools
 	if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 		cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
 				-DBOOST_ROOT=$BOOST_ROOT \
-				-DCMAKE_POSITION_INDEPENDENT_CODE=on \
-				-DCMAKE_INSTALL_PREFIX=$HYPERSCAN_ROOT
+				-DCMAKE_POSITION_INDEPENDENT_CODE=on
 	else
 		cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
 				-DBOOST_ROOT=$BOOST_ROOT \
 				-DCMAKE_POSITION_INDEPENDENT_CODE=on \
-				-DCMAKE_INSTALL_PREFIX=$HYPERSCAN_ROOT \
 				-DCMAKE_C_COMPILER=/usr/bin/gcc-4.8 \
 				-DCMAKE_CXX_COMPILER=/usr/bin/g++-4.8
 	fi
+
 	make
-	make install
 else
 	echo "Using cached hyperscan v${HYPERSCAN_VERSION} @ ${HYPERSCAN_ROOT}.";
-
-	cd $HYPERSCAN_ROOT
-
-	make install
 fi
+
+cd $HYPERSCAN_ROOT
+
+make install
