027427b837c564c9dd2ba7cc0c7e7921005c33db
Flier Lu <flier.lu@gmail.com>
2018-08-01 14:16:31 +0800

    use system gcc/g++

--- .travis/build-hyperscan.sh
+++ .travis/build-hyperscan.sh
@@ -7,19 +7,10 @@ if [ ! -f "$HYPERSCAN_ROOT/lib/libhs.a" ]; then
 	tar -xzf /tmp/hyperscan.tar.gz -C /tmp/hyperscan --strip-components 1
 	cd /tmp/hyperscan
 	rm -rf tools
-	if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-		cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-				-DCMAKE_INSTALL_PREFIX=$HYPERSCAN_ROOT \
-				-DBOOST_ROOT=$BOOST_ROOT \
-				-DCMAKE_POSITION_INDEPENDENT_CODE=on
-	else
-		cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-				-DBOOST_ROOT=$BOOST_ROOT \
-				-DCMAKE_INSTALL_PREFIX=$HYPERSCAN_ROOT \
-				-DCMAKE_POSITION_INDEPENDENT_CODE=on \
-				-DCMAKE_C_COMPILER=/usr/bin/gcc-4.8 \
-				-DCMAKE_CXX_COMPILER=/usr/bin/g++-4.8
-	fi
+
+	cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+			-DCMAKE_INSTALL_PREFIX=$HYPERSCAN_ROOT \
+			-DBUILD_STATIC_AND_SHARED=on
 
 	make
 	make install
