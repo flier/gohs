bd2c887f6dc4687106087f7f0d6b805adee45024
Flier Lu <flier.lu@gmail.com>
2019-05-14 18:23:42 +0800

    remove hyperscan cache

--- .travis/build-hyperscan.sh
+++ .travis/build-hyperscan.sh
@@ -1,20 +1,15 @@
 #/bin/sh -f
 set -e
 
-if [ ! -f "$HYPERSCAN_ROOT/lib/libhs.so" ]; then
-	wget https://github.com/intel/hyperscan/archive/v$HYPERSCAN_VERSION.tar.gz -O /tmp/hyperscan.tar.gz
-	mkdir -p /tmp/hyperscan
-	tar -xzf /tmp/hyperscan.tar.gz -C /tmp/hyperscan --strip-components 1
-	cd /tmp/hyperscan
-	rm -rf tools
+wget https://github.com/intel/hyperscan/archive/v$HYPERSCAN_VERSION.tar.gz -O /tmp/hyperscan.tar.gz
+mkdir -p /tmp/hyperscan
+tar -xzf /tmp/hyperscan.tar.gz -C /tmp/hyperscan --strip-components 1
+cd /tmp/hyperscan
+rm -rf tools
 
-	cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-			-DBUILD_STATIC_AND_SHARED=on \
-			-DCMAKE_POSITION_INDEPENDENT_CODE=on \
-			-DCMAKE_INSTALL_PREFIX=$HYPERSCAN_ROOT
+cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+		-DBUILD_STATIC_AND_SHARED=on \
+		-DCMAKE_POSITION_INDEPENDENT_CODE=on
 
-	make
-	make install
-else
-	echo "Using cached hyperscan v${HYPERSCAN_VERSION} @ ${HYPERSCAN_ROOT}.";
-fi
+make
+make install
