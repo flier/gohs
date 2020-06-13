8dba804e60a1662e3216c4a15c3dbf064839798d
Flier Lu <flier.lu@gmail.com>
2020-06-13 22:05:46 +0800

    extract examples

--- hyperscan/compile_test.go
+++ hyperscan/compile_test.go
@@ -1,46 +1,10 @@
 package hyperscan
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
-
-func ExampleParsePattern() {
-	p, err := ParsePattern("3:/foobar/i8")
-
-	fmt.Println(err)
-	fmt.Println(p.Id)
-	fmt.Println(p.Expression)
-	fmt.Println(p.Flags)
-	// Output:
-	// <nil>
-	// 3
-	// foobar
-	// 8i
-}
-
-func ExampleParsePatterns() {
-	patterns, err := ParsePatterns(strings.NewReader(`
-# empty line and comment will be skipped
-
-1:/hatstand.*teakettle/s
-2:/(hatstand|teakettle)/iH
-3:/^.{10,20}hatstand/m	
-`))
-
-	fmt.Println(err)
-	for _, p := range patterns {
-		fmt.Println(p)
-	}
-	// Output:
-	// <nil>
-	// 1:/hatstand.*teakettle/s
-	// 2:/(hatstand|teakettle)/Hi
-	// 3:/^.{10,20}hatstand/m
-}
 
 func TestPattern(t *testing.T) {
 	Convey("Give a pattern", t, func() {
---
+++ hyperscan/example_compile_test.go
@@ -0,0 +1,44 @@
+package hyperscan_test
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/flier/gohs/hyperscan"
+)
+
+// This example demonstrates parsing pattern with id and flags
+func ExampleParsePattern() {
+	p, err := hyperscan.ParsePattern("3:/foobar/i8")
+
+	fmt.Println(err)
+	fmt.Println(p.Id)
+	fmt.Println(p.Expression)
+	fmt.Println(p.Flags)
+	// Output:
+	// <nil>
+	// 3
+	// foobar
+	// 8i
+}
+
+// This example demonstrates parsing patterns with comment
+func ExampleParsePatterns() {
+	patterns, err := hyperscan.ParsePatterns(strings.NewReader(`
+# empty line and comment will be skipped
+
+1:/hatstand.*teakettle/s
+2:/(hatstand|teakettle)/iH
+3:/^.{10,20}hatstand/m	
+`))
+
+	fmt.Println(err)
+	for _, p := range patterns {
+		fmt.Println(p)
+	}
+	// Output:
+	// <nil>
+	// 1:/hatstand.*teakettle/s
+	// 2:/(hatstand|teakettle)/Hi
+	// 3:/^.{10,20}hatstand/m
+}
